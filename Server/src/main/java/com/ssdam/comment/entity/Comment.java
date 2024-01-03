5256da92d948d7e2196b43bfc82a2a8c63c9a300
hyezuu <duudu0704@gmail.com>
2024-01-03 14:16:24 +0900

    update comment entity 수정

--- Server/src/main/java/com/ssdam/comment/entity/Comment.java
+++ Server/src/main/java/com/ssdam/comment/entity/Comment.java
@@ -1,13 +1,13 @@
 package com.ssdam.comment.entity;
 
+import com.ssdam.member.entity.Member;
+import com.ssdam.party.entity.Party;
+import com.ssdam.reply.entity.Reply;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -17,4 +17,18 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long commentId;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PARTY_ID")
+    private Party party;
+
+    @OneToOne
+    private Reply reply;
 }
