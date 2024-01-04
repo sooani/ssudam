f218c2140fb4956710062b9dd30446f5c00c28b5
anminjoo <oeoe52@kakao.com>
2024-01-04 16:06:00 +0900

    마이페이지/ 프로필 수정/ 회원탈퇴 모달 파일 만들기

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import Meeting from "./components/Meeting/Meeting";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MakePost from "./pages/MakePost";
+import LeaveModal from "./pages/LeaveModal";//이거삭제
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/meetings/new" element={<MakePost />} />
       </Routes>
+      <LeaveModal />
     </BrowserRouter>
   );
 }
---
+++ client/src/pages/MyPage.jsx
@@ -0,0 +1,5 @@
+//MyPage.jsx
+//마이페이지 화면입니다. (작업자:안민주)
+
+import React from 'react';
+
