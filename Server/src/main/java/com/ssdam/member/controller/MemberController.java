ae5a72a6e1c2f089d3e6b315763cbfba4a1f8c2d
JUNHA CHOI <choijh0309@naver.com>
2024-01-04 23:06:31 +0900

    update Member 기본 완성

--- Server/src/main/java/com/ssdam/member/controller/MemberController.java
+++ Server/src/main/java/com/ssdam/member/controller/MemberController.java
@@ -1,16 +1,24 @@
 package com.ssdam.member.controller;
 
+import com.ssdam.dto.MultiResponseDto;
+import com.ssdam.dto.SingleResponseDto;
+import com.ssdam.member.dto.MemberPatchDto;
+import com.ssdam.member.dto.MemberPostDto;
+import com.ssdam.member.entity.Member;
 import com.ssdam.member.mapper.MemberMapper;
 import com.ssdam.member.service.MemberService;
+import com.ssdam.utils.UriCreator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/members")
@@ -27,5 +35,49 @@ public class MemberController {
     }
 
     @PostMapping
-    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto)
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
+        Member member = memberService.createMember(mapper.memberPostDtoToMember(memberPostDto));
+        URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, member.getMemberId());
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+
+        Member member =
+                memberService.updateMember(mapper.memberPatchDtoToMember(memberPatchDto));
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(mapper.memberToMemberResponseDto(member)),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId) {
+        Member member = memberService.findMember(memberId);
+        return new ResponseEntity(
+                new SingleResponseDto<>(mapper.memberToMemberResponseDto(member)),
+                HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getMembers(@Positive @RequestParam int page,
+                                     @Positive @RequestParam int size) {
+        Page<Member> pageMembers = memberService.findMembers(page - 1, size);
+        List<Member> members = pageMembers.getContent();
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(mapper.membersToMemberResponseDto(members), pageMembers),
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("{member-id}")
+    public ResponseEntity deleteMember(
+            @PathVariable("member-id") @Positive long memberId) {
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- Server/src/main/java/com/ssdam/member/dto/MemberPostDto.java
+++ Server/src/main/java/com/ssdam/member/dto/MemberPostDto.java
@@ -1,4 +1,17 @@
 package com.ssdam.member.dto;
 
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
 public class MemberPostDto {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank(message = "닉네임은 공백이 아니어야 합니다.")
+    private String nickname;
 }
