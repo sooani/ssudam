33e42f4e608d33baa063b85e9ebfca5fc45b5f68
JUNHA CHOI <choijh0309@naver.com>
2024-01-06 23:06:37 +0900

    update password @Pattern 추가

--- Server/src/main/java/com/ssdam/member/dto/MemberPostDto.java
+++ Server/src/main/java/com/ssdam/member/dto/MemberPostDto.java
@@ -13,6 +13,8 @@ public class MemberPostDto {
     private String email;
 
     @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.")
     private String password1;
 
     @NotBlank(message = "비밀번호는 확인은 필수 항목입니다.")
