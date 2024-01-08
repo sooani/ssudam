34652ac9872f0162e48518b5abfe97f3d54fa4bc
anminjoo <oeoe52@kakao.com>
2024-01-08 09:21:17 +0900

    프로필수정,css 생성

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -13,34 +13,34 @@ import classes from  '../styles/pages/MyPage.module.css';
 //{classes.}
 const MyPage = () => {
     return (
-        <div>
-            <Header />
-            <div className={classes.MypageContainer}>
-                <div className={classes.MyInfo}>
-                    <div className={classes.Myprofile}>
-                        <h1 className={classes.ProfileHeader}>내 프로필</h1>
-                        <div className={classes.ProfileImageContainer}>
-                        <img alt="ProfileImage" src={footerLogo} width="50px" height="50px"/>
+        <div className={classes.Mypageroom}>
+                <Header />
+                <div className={classes.MypageContainer}>
+                    <div className={classes.MyInfo}>
+                        <div className={classes.Myprofile}>
+                            <h1 className={classes.ProfileHeader}>내 프로필</h1>
+                            <div className={classes.ProfileImageContainer}>
+                            <img alt="ProfileImage" src={footerLogo} width="50px" height="50px"/>
+                            </div>
+                            <Link to="/edit-profile" className="Btn_EditProfile">회원정보수정</Link>
                         </div>
-                        <Link to="/edit-profile" className="Btn_EditProfile">회원정보수정</Link>
+                        <div className={classes.MyData}>
+                            <div className={classes.NicknameContainer}>
+                                <p className={classes.Nickname}>닉네임</p>
+                                <div className={classes.UserNickname}>유저의 닉네임이 출력됩니다.</div>
+                            <div className={classes.EmailContainer}>
+                                <p className={classes.Email}>이메일</p>
+                                <div className={classes.UserEmail}>유저의 이메일이 출력됩니다.</div>
+                            </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={classes.MyData}>
-                        <div className={classes.NicknameContainer}>
-                            <p className={classes.Nickname}>닉네임</p>
-                            <div className={classes.UserNickname}>유저의 닉네임이 출력됩니다.</div>
-                        <div className={classes.EmailContainer}>
-                            <p className={classes.Email}>이메일</p>
-                            <div className={classes.UserEmail}>유저의 이메일이 출력됩니다.</div>
-                        </div>
-                        </div>
+                    <div className={classes.MyEvent}>
+                        <h1 className={classes.MyEventHeader}>내가 참여한 모임</h1>
+                        {/* 내가 참여한 모임 나타내기 */}
                     </div>
                 </div>
-                <div className={classes.MyEvent}>
-                    <h1 className={classes.MyEventHeader}>내가 참여한 모임</h1>
-                    {/* 내가 참여한 모임 나타내기 */}
-                </div>
-            </div>
-            <Footer />
+                <Footer />
         </div>
     )
 }
