12ec2d3eaaa3842b417fe32906ad2be38dc7d457
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2024-01-09 09:33:47 +0900

    게시글 클릭 시 모달창 띄움

--- client/src/components/MainPage/ListCard.jsx
+++ client/src/components/MainPage/ListCard.jsx
@@ -1,19 +1,24 @@
 import classes from '../../styles/components/ListCard.module.css';
 import footerLogo from '../../images/footerLogo.png';
 import dummy from '../../dummyCard.json';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import SignUpModal from '../../pages/SignUpModal';
 /*
     새로운 모임을 위한 게시글 컴포넌트
     더미 데이터를 이용하여 확인 입니다.
 */
 
 const ListCard = () => {
-  const navigate = useNavigate();
-  const gotoSignUpModal = () => {
-    navigate('/SignUpModal');
+  // const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const handlePostClick = () => {
+    // 게시글 클릭 시 모달 열기
+    setModalIsOpen(true);
   };
+
   return (
-    <div className={classes.ListCard} onClick={gotoSignUpModal}>
+    <div className={classes.ListCard} onClick={handlePostClick}>
       <img src={footerLogo} alt="게시판 이미지" />
       <div className={classes.ListCardTitle}>
         <span>{dummy.title}</span>
@@ -24,6 +29,7 @@ const ListCard = () => {
       <div className={classes.ListCardMeetingDate}>
         <span>{dummy.meetingDate}</span>
       </div>
+      <SignUpModal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
     </div>
   );
 };
