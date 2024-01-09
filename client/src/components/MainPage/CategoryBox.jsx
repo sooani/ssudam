6a0fcade2b3c866ab3c7faa48bbd219212031565
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2024-01-10 00:47:46 +0900

    제이슨 서버 만든 데이터 게시글 보임, 이제 탭에 맞게 기능 설정 해야 한다.

--- client/src/components/MainPage/CategoryBox.jsx
+++ client/src/components/MainPage/CategoryBox.jsx
@@ -1,33 +1,40 @@
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import ListCard from './ListCard';
 import classes from '../../styles/components/CategoryBox.module.css';
 
-const CategoryBox = () => {
+const ITEMS_PER_PAGE = 12;
+
+const CategoryBox = ({ data, activeTab }) => {
+  const filteredData = data.filter((item) =>
+    activeTab === 'recruiting'
+      ? item.partyStatus === 'PARTY_OPENED'
+      : item.partyStatus !== 'PARTY_OPENED'
+  );
+  const [currentPage, setCurrentPage] = useState(0);
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+  const pageCount = Math.ceil(filteredData.length / ITEMS_PER_PAGE);
+  const offset = currentPage * ITEMS_PER_PAGE;
+
+  const currentData = filteredData.slice(offset, offset + ITEMS_PER_PAGE);
   return (
     <div className={classes.listCardContainer}>
-      <div className={classes.list1}>
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-      </div>
-      <div className={classes.list2}>
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-      </div>
-      <div className={classes.list3}>
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-      </div>
-      <div className={classes.list4}>
-        <ListCard />
-        <ListCard />
-        <ListCard />
-        <ListCard />
-      </div>
+      {currentData.map((party) => (
+        <ListCard party={party} />
+      ))}
+      <ReactPaginate
+        previousLabel={'이전'}
+        nextLabel={'다음'}
+        breakLabel={'...'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 };
--- client/src/components/MainPage/ListCard.jsx
+++ client/src/components/MainPage/ListCard.jsx
@@ -1,16 +1,13 @@
+import React, { useState } from 'react';
 import classes from '../../styles/components/ListCard.module.css';
 import footerLogo from '../../images/footerLogo.png';
-import dummy from '../../dummyCard.json';
-// import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import SignUpModal from '../../pages/SignUpModal';
 /*
     새로운 모임을 위한 게시글 컴포넌트
     더미 데이터를 이용하여 확인 입니다.
 */
 
-const ListCard = () => {
-  // const navigate = useNavigate();
+const ListCard = ({ party }) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const handlePostClick = () => {
     // 게시글 클릭 시 모달 열기
@@ -20,15 +17,17 @@ const ListCard = () => {
   return (
     <div className={classes.ListCard} onClick={handlePostClick}>
       <img src={footerLogo} alt="게시판 이미지" />
+
       <div className={classes.ListCardTitle}>
-        <span>{dummy.title}</span>
+        <span>{party?.title}</span>
       </div>
       <div className={classes.ListCardContent}>
-        <span>{dummy.content}</span>
+        <span>{party?.content}</span>
       </div>
       <div className={classes.ListCardMeetingDate}>
-        <span>{dummy.meetingDate}</span>
+        <span>{party?.meetingDate}</span>
       </div>
+
       <SignUpModal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
     </div>
   );
