76d9d24ec6396e2f142f70a76358d8c9af7ca387
JUNHA CHOI <choijh0309@naver.com>
2024-01-12 20:40:31 +0900

    update "update MemberPatchTest 구현"

--- Server/src/test/java/com/ssdam/member/controller/MemberControllerTest.java
+++ Server/src/test/java/com/ssdam/member/controller/MemberControllerTest.java
@@ -3,6 +3,7 @@ package com.ssdam.member.controller;
 import com.google.gson.Gson;
 import com.ssdam.member.dto.MemberPatchDto;
 import com.ssdam.member.dto.MemberPostDto;
+import com.ssdam.member.dto.MemberResponseDto;
 import com.ssdam.member.entity.Member;
 import com.ssdam.member.mapper.MemberMapper;
 import com.ssdam.member.service.MemberService;
@@ -30,11 +31,12 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MemberController.class)
 @MockBean(JpaMetamodelMappingContext.class)
@@ -96,5 +98,64 @@ class MemberControllerTest {
                 ));
     }
 
+    @Test
+    public void patchMemberTest() throws Exception {
+        // given
+        long memberId = 1L;
+        MemberPatchDto patch = new MemberPatchDto(memberId, "abc123!@", "abc123!@", "보노보노개구리", Member.MemberStatus.MEMBER_ACTIVE);
+        String content = gson.toJson(patch);
 
+        MemberResponseDto responseDto =
+                new MemberResponseDto(1L,
+                        "cjh@gmail.com",
+                        "보노보노개구리",
+                        Member.MemberStatus.MEMBER_ACTIVE);
+
+        given(mapper.memberPatchDtoToMember(Mockito.any(MemberPatchDto.class))).willReturn(new Member());
+
+        given(memberService.updateMember(Mockito.any(Member.class))).willReturn(new Member());
+
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(responseDto);
+
+        // when
+        ResultActions actions =
+                mockMvc.perform(
+                        patch("/v1/members/{member-id}", memberId)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                );
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.memberId").value(patch.getMemberId()))
+                .andExpect(jsonPath("$.data.nickname").value(patch.getNickname()))
+                .andExpect(jsonPath("$.data.memberStatus").value(patch.getMemberStatus().getStatus()))
+                .andDo(document("patch-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("member-id").description("회원 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").ignored(),
+                                        fieldWithPath("password").type(JsonFieldType.STRING).description("패스워드").optional(),
+                                        fieldWithPath("confirmPassword").type(JsonFieldType.STRING).description("패스워드 확인").optional(),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임").optional(),
+                                        fieldWithPath("memberStatus").type(JsonFieldType.STRING).description("회원 상태: MEMBER_ACTIVE / MEMBER_SLEEP / MEMBER_QUIT").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.nickname").type(JsonFieldType.STRING).description("닉네임"),
+                                        fieldWithPath("data.memberStatus").type(JsonFieldType.STRING).description("회원 상태: 활동중 / 휴면 상태 / 탈퇴 상태")
+                                )
+                        )
+                ));
+    }
 }
