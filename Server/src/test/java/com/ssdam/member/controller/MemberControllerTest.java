9f2e9d3b732b53fd7e50f1ea411ce9ebbf6b742d
JUNHA CHOI <choijh0309@naver.com>
2024-01-12 16:08:39 +0900

    update "update MemberPostTest 구현"

---
+++ Server/src/test/java/com/ssdam/member/controller/MemberControllerTest.java
@@ -0,0 +1,100 @@
+package com.ssdam.member.controller;
+
+import com.google.gson.Gson;
+import com.ssdam.member.dto.MemberPatchDto;
+import com.ssdam.member.dto.MemberPostDto;
+import com.ssdam.member.entity.Member;
+import com.ssdam.member.mapper.MemberMapper;
+import com.ssdam.member.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static com.ssdam.utils.ApiDocumentUtils.getRequestPreProcessor;
+import static com.ssdam.utils.ApiDocumentUtils.getResponsePreProcessor;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
+@TestPropertySource(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration")
+class MemberControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private MemberMapper mapper;
+
+    @Autowired
+    private Gson gson;
+
+    @Test
+    public void postMemberTest() throws Exception {
+        // given
+        MemberPostDto post = new MemberPostDto("cjh@gmail.com", "code123!@", "code123!@", "보노보노의습격단");
+        String content = gson.toJson(post);
+
+        given(mapper.memberPostDtoToMember(Mockito.any(MemberPostDto.class))).willReturn(new Member());
+
+        Member mockResultMember = new Member();
+        mockResultMember.setMemberId(1L);
+        given(memberService.createMember(Mockito.any(Member.class))).willReturn(mockResultMember);
+
+        // when
+        ResultActions actions =
+                mockMvc.perform(
+                        post("/v1/members")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                );
+
+        // then
+        actions
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", is(startsWith("/v1/members/"))))
+                .andDo(document(
+                        "post-member",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("password").type(JsonFieldType.STRING).description("패스워드"),
+                                        fieldWithPath("confirmPassword").type(JsonFieldType.STRING).description("패스워드 확인"),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임")
+                                )
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("Location header. 등록된 리소스의 URI")
+                        )
+                ));
+    }
+
+
+}
