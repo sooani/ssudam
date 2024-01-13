c5358d18f980fb266488bece8f84330c7a48a71b
hyezuu <duudu0704@gmail.com>
2024-01-13 20:10:18 +0900

    test BookmarkController

--- Server/src/main/java/com/ssdam/bookmark/controller/BookmarkController.java
+++ Server/src/main/java/com/ssdam/bookmark/controller/BookmarkController.java
@@ -1,10 +1,13 @@
 package com.ssdam.bookmark.controller;
 
 import com.ssdam.bookmark.service.BookmarkService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/bookmarks")
@@ -23,9 +26,11 @@ public class BookmarkController {
     }
 
     @GetMapping("/parties/{party-id}/bookmark-status")
-    public ResponseEntity<Boolean> checkBookmarkStatus(@PathVariable("party-id")@Positive long partyId,
+    public ResponseEntity checkBookmarkStatus(@PathVariable("party-id")@Positive long partyId,
                                                        @RequestParam @Positive long memberId){
         boolean isBookmarked = bookmarkService.isPartyBookmarkedByUser(memberId,partyId);
-        return ResponseEntity.ok(isBookmarked);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("isBookmarked", isBookmarked);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
---
+++ Server/src/test/java/com/ssdam/bookmark/controller/BookmarkControllerTest.java
@@ -0,0 +1,98 @@
+package com.ssdam.bookmark.controller;
+
+import com.ssdam.bookmark.service.BookmarkService;
+import com.ssdam.like.controller.LikeController;
+import com.ssdam.like.service.LikeService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BookmarkController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
+@TestPropertySource(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration")
+public class BookmarkControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BookmarkService bookmarkService;
+
+    @Test
+    public void toggleBookmarkToPartyTest() throws Exception {
+        //given
+        long partyId = 1L;
+        long memberId = 1L;
+
+        doNothing().when(bookmarkService).toggleBookmark(Mockito.anyLong(), Mockito.anyLong());
+        //when
+        ResultActions actions = mockMvc.perform(
+                post("/v1/bookmarks/parties/{party-id}", partyId)
+                        .param("memberId", String.valueOf(memberId)));
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andDo(document("toggle-bookmark-to-party",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("party-id").description("모집글 식별자")
+                        ),
+                        requestParameters(
+                                parameterWithName("memberId").description("회원 식별자 ID")
+                        )));
+    }
+
+    @Test
+    public void checkBookmarkStatusTest() throws Exception {
+        //given
+        long partyId = 1L;
+        long memberId = 1L;
+        boolean isBookmarked = true;
+
+        given(bookmarkService.isPartyBookmarkedByUser(Mockito.anyLong(), Mockito.anyLong())).willReturn(isBookmarked);
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/v1/bookmarks/parties/{party-id}/bookmark-status", partyId)
+                        .param("memberId", String.valueOf(memberId))
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isBookmarked").value(isBookmarked))
+                .andDo(document("check-bookmark-status",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("party-id").description("모집글 식별자")
+                        ),
+                        requestParameters(
+                                parameterWithName("memberId").description("회원 식별자 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("isBookmarked").type(JsonFieldType.BOOLEAN).description("북마크 여부"))
+                ));
+    }
+
+}
