95c5a2ae8aebf9206c26e42ffbbfe7b162fc3d92
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2024-01-17 14:25:14 +0900

    셋업파일 삭제

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: "http://15.164.28.18:8080",
+  // baseURL: 'http://3.36.97.234:8080',
+  baseURL: 'http://localhost:8080',
   withCredentials: true,
 });
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("Authorization");
+    const token = localStorage.getItem('Authorization');
 
     if (token) {
       config.headers.Authorization = token;
