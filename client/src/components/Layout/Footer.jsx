ffc161c51d9590790c7fbf3c5ecb92c65dfc2edf
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2024-01-19 10:23:54 +0900

    useAxiosInstance 변경 수정

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,19 +1,17 @@
 import classes from '../../styles/components/Footer.module.css';
-import footerLogo from '../../images/footerLogo.png';
+// import footerLogo from '../../images/footerLogo.png';
 
 /*
   푸터는 간단하게 카피라이트만 나옴
 */
 const Footer = (props) => {
   return (
-    <footer>
-      <div className={classes.footer}>
-        <div className={classes.logoContainer}>
-          <img src={footerLogo} alt="footerLogo" />
-          <p>쓰담</p>
-        </div>
-        <p>© {new Date().getFullYear()} 쓰담. All rights reserved.</p>
+    <footer className={classes.footer}>
+      <div className={classes.logoContainer}>
+        {/* <img src={footerLogo} alt="footerLogo" /> */}
+        <p>쓰담</p>
       </div>
+      <p>© {new Date().getFullYear()} 쓰담. All rights reserved.</p>
     </footer>
   );
 };
