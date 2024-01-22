1fb68d8b070ef0cc5e3eb5b29578a8582fe19e45
hyehye225 <h3192@naver.com>
2024-01-22 11:20:01 +0900

    투두리스트 css 조정 & 모달 안닫힘 이슈 해결

--- client/src/components/MainPage/ListCard.jsx
+++ client/src/components/MainPage/ListCard.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import classes from '../../styles/components/ListCard.module.css';
-import SignUpModal from '../../pages/SignUpModal';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../features/userSlice';
-import { AiFillAlert } from 'react-icons/ai';
-import { FaBookmark } from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import classes from "../../styles/components/ListCard.module.css";
+import SignUpModal from "../../pages/SignUpModal";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { AiFillAlert } from "react-icons/ai";
+import { FaBookmark } from "react-icons/fa";
 
 /*
     게시글 컴포넌트
@@ -49,11 +49,11 @@ const ListCard = ({ party, meetingId, loggedInUser }) => {
       const daysLeft = Math.floor(timeDifference / (1000 * 3600 * 24));
       // 마감이 지났는지 확인
       const isClosed = timeDifference < 0;
-      let timeLeftText = '';
+      let timeLeftText = "";
       if (isClosed) {
-        timeLeftText = '모집 마감';
+        timeLeftText = "모집 마감";
       } else if (daysLeft === 0) {
-        timeLeftText = '오늘 마감';
+        timeLeftText = "오늘 마감";
       } else {
         timeLeftText = `마감 ${daysLeft}일 전`;
       }
@@ -80,42 +80,44 @@ const ListCard = ({ party, meetingId, loggedInUser }) => {
   const extractDate = (fullDate) => {
     const dateObject = new Date(fullDate);
     // 날짜를 'YYYY-MM-DD' 형식으로 변환
-    const formattedDate = dateObject.toISOString().split('T')[0];
+    const formattedDate = dateObject.toISOString().split("T")[0];
     return formattedDate;
   };
   return (
-    <div className={classes.listCard} onClick={handlePostClick}>
-      <div className={classes.top}>
-        <div className={classes.new}>
-          <div className={classes.newPost}>
-            {isNewPost && <span>최근 게시물</span>}
-          </div>
-          {/* 남은 날짜 및 시간 표시 */}
-          <div className={classes.listCardTimeLeft}>
-            <AiFillAlert className={classes.aiFillAlert} />
-            {timeLeft !== null && <span>{timeLeft}</span>}
+    <>
+      <div className={classes.listCard} onClick={handlePostClick}>
+        <div className={classes.top}>
+          <div className={classes.new}>
+            <div className={classes.newPost}>
+              {isNewPost && <span>최근 게시물</span>}
+            </div>
+            {/* 남은 날짜 및 시간 표시 */}
+            <div className={classes.listCardTimeLeft}>
+              <AiFillAlert className={classes.aiFillAlert} />
+              {timeLeft !== null && <span>{timeLeft}</span>}
+            </div>
           </div>
         </div>
-      </div>
-      <div className={classes.listCardMeetingDate}>
-        <span>모임날짜 | {extractDate(party?.meetingDate)}</span>
-      </div>
-      <div className={classes.listCardClosingDate}>
-        <span>마감일 | {extractDate(party?.closingDate)}</span>
-      </div>
-      {/* <div className={classes.listCardTitle}> */}
-      <h1 className={classes.title}>{party?.title}</h1>
-      {/* </div> */}
-      <div className={classes.bottom}>
-        <div className={classes.capacity}>
-          <span className={classes.capacityNum}>모집인원 :</span>
-          <div className={classes.current}>
-            {party?.currentCapacity} / {party?.maxCapacity}
+        <div className={classes.listCardMeetingDate}>
+          <span>모임날짜 | {extractDate(party?.meetingDate)}</span>
+        </div>
+        <div className={classes.listCardClosingDate}>
+          <span>마감일 | {extractDate(party?.closingDate)}</span>
+        </div>
+        {/* <div className={classes.listCardTitle}> */}
+        <h1 className={classes.title}>{party?.title}</h1>
+        {/* </div> */}
+        <div className={classes.bottom}>
+          <div className={classes.capacity}>
+            <span className={classes.capacityNum}>모집인원 :</span>
+            <div className={classes.current}>
+              {party?.currentCapacity} / {party?.maxCapacity}
+            </div>
           </div>
         </div>
       </div>
       <SignUpModal isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)} />
-    </div>
+    </>
   );
 };
 
