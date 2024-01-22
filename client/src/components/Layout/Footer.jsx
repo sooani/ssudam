1b17da481565542bdbb1fb8c58d07f26e575df20
hyehye225 <h3192@naver.com>
2024-01-22 09:57:43 +0900

    footer 수정

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -1,17 +1,19 @@
-import classes from '../../styles/components/Footer.module.css';
-// import footerLogo from '../../images/footerLogo.png';
+import classes from "../../styles/components/Footer.module.css";
+import footerLogo from "../../images/logo.png";
 
 /*
   푸터는 간단하게 카피라이트만 나옴
 */
 const Footer = (props) => {
   return (
-    <footer className={classes.footer}>
-      <div className={classes.logoContainer}>
-        {/* <img src={footerLogo} alt="footerLogo" /> */}
-        <p>쓰담</p>
+    <footer className={classes.container}>
+      <img src={footerLogo} alt="footerLogo" height={"80px"} weight={"80px"} />
+      <div className={classes.footer}>
+        <div className={classes.logoContainer}>
+          <p>쓰담</p>
+        </div>
+        <p>© {new Date().getFullYear()} 쓰담. All rights reserved.</p>
       </div>
-      <p>© {new Date().getFullYear()} 쓰담. All rights reserved.</p>
     </footer>
   );
 };
