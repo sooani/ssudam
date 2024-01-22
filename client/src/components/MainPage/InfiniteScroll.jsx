6523fdd435065b323c1bc227b68317d695178991
김윤한 <88180966+YunHanKIM@users.noreply.github.com>
2024-01-22 13:11:42 +0900

    흠... 무한스크롤이 아니라 데이터 한번에 불러 오고있었네..

--- client/src/components/MainPage/InfiniteScroll.jsx
+++ client/src/components/MainPage/InfiniteScroll.jsx
@@ -1,27 +1,35 @@
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const InfiniteScroll = ({ onScrollEnd }) => {
+  const containerRef = useRef(null);
+
   const handleScroll = () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-    // 스크롤이 바닥에 닿았는지 여부를 계산
-    const bottomOfWindow = scrollTop + clientHeight >= scrollHeight;
-
-    if (bottomOfWindow) {
-      onScrollEnd();
+    const container = containerRef.current;
+    if (container) {
+      const { scrollTop, scrollHeight, clientHeight } = container;
+      if (scrollTop + clientHeight >= scrollHeight - 10) {
+        // 스크롤이 끝에 도달했을 때 onScrollEnd 함수 호출
+        onScrollEnd();
+      }
     }
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-
-    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener('scroll', handleScroll);
+      return () => {
+        container.removeEventListener('scroll', handleScroll);
+      };
+    }
   }, [onScrollEnd]);
 
-  return null;
+  return (
+    <div
+      ref={containerRef}
+      style={{ overflowY: 'auto', maxHeight: '500px' /* 원하는 높이 설정 */ }}
+    />
+  );
 };
 
 export default InfiniteScroll;
